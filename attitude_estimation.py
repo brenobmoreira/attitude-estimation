34edcdbf75100966755ba79a8a7eb8ce6db0554b
brenobmoreira <brenobmoreira20@gmail.com>
2023-04-21 18:03:09 -0300

    all angles of pitch and roll estimated

--- attitude_estimation.py
+++ attitude_estimation.py
@@ -1,44 +1,54 @@
 import numpy as np
+from datetime import datetime
 
 ## roll fi, pitch theta
 
 def roll_angle(Gpy, Gpz):
-    roll = Gpy/Gpz
-    fi = np.arctan(roll)*180/np.pi ## converting to arctan and rad to grad
-    if(fi > 0):
-        fi2 = 180 - fi
-    else:
-        fi2 = 180 + fi
-    #print(Gpy/Gpz, ' -- Gpy/Gpz')
-    #print(np.arctan(Gpy/Gpz)*180/np.pi, '--Arctan')
-    return round(fi, 4), round(fi2, 4)
+    fi = np.arctan2(Gpy, Gpz)*180/np.pi ## converting to arctan and rad to grad
+    
+    return round(fi, 4)
 
 def pitch_angle(Gpx, Gpy, Gpz):
     pitch = (-Gpx)/np.sqrt(Gpy**2+Gpz**2) 
     theta = np.arctan(pitch)*180/np.pi ## converting to arctan and rad to grad
     
-    #print(theta, ' -- -Gpx/sqrt(Gpy^2+Gpz^2)')
-    #print(np.arctan(theta)*180/np.pi, '--Arctan')
     return round(theta, 4)
 
+## Organizating the data 
 
 data = []
+organizated = []
 attitude = 'attitude_exam.log'
 
 with open(attitude) as f:
     f = f.readlines()
 
-#for line in f:
-#    for phrase in line:
-#        data.append(line)
-#        break
+for line in f:
+    for phrase in line:
+        data.append(line)
+        break
 
-Gpx = 0.461105
-Gpy = 0.082198
-Gpz = -0.887432
-fi, fi2 = roll_angle(Gpy, Gpz)
+organizated = [element.strip().split(';') for element in data]
 
-print(fi, fi2)
-print(pitch_angle(Gpx, Gpy, Gpz))
+## calculating all the angles
 
-print(data)
+for i in organizated:
+    time_stamp = datetime.fromtimestamp(int(i[0]))
+    Gpx = int(i[1])
+    Gpy = int(i[2])
+    Gpz = int(i[3])
+
+    print('For time: '  , time_stamp)
+    print('Gpx, Gpy and Gpz: ')
+    print(Gpx)
+    print(Gpy)
+    print(Gpz)
+    print('Rolls angles: ')
+    fi = roll_angle(Gpy, Gpz)
+    print(fi)
+    print('Pitch angle: ')
+    theta = pitch_angle(Gpx, Gpy, Gpz)
+    print(theta)
+    print('\n')
+    
+    
